79c8a3331e879104b81bb51dd5292736e7b46896
Nicha <Nicha.yhupaet@gmail.com>
2017-04-14 14:46:19 +0700

    Add activity daily journal, setting

--- app/src/main/java/com/example/nicha/finalproject/TrackingActivity.java
+++ app/src/main/java/com/example/nicha/finalproject/TrackingActivity.java
@@ -1,9 +1,15 @@
 package com.example.nicha.finalproject;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
 import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import static com.example.nicha.finalproject.R.id.tvBike;
 import static com.example.nicha.finalproject.R.id.tvWalk;
@@ -12,12 +18,12 @@ public class TrackingActivity extends AppCompatActivity {
 
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tracking);
 
-
     }
+
+
 }
