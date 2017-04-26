209af92852adec1265822ede1b3f2db91a1b0310
Nicha <Nicha.yhupaet@gmail.com>
2017-04-27 01:31:26 +0700

    Add function dewy reacts

--- app/src/main/java/com/example/nicha/finalproject/DewyActivity.java
+++ app/src/main/java/com/example/nicha/finalproject/DewyActivity.java
@@ -4,10 +4,13 @@ import android.graphics.PorterDuff;
 import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 
 public class DewyActivity extends AppCompatActivity {
@@ -15,12 +18,22 @@ public class DewyActivity extends AppCompatActivity {
 //    private ImageView egg;
     ImageView monster;
     ImageButton btnFeed;
+    private ProgressBar pgExp = null;
+    private int i = 0;
+    private int j = 1;
+    TextView tvLevel;
+    ImageView ivHeart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dewy);
-
+        pgExp = (ProgressBar)findViewById(R.id.pgExp);
+        tvLevel = (TextView) findViewById(R.id.tvLevel);
+        ivHeart = (ImageView) findViewById(R.id.ivHeart);
+        ivHeart.setVisibility(View.GONE);
+        tvLevel.setText("Lv "+j);
+        pgExp.setMax(100);
 //        egg = (ImageView) findViewById(R.id.egg);
         monster = (ImageView) findViewById(R.id.monster);
 //
@@ -41,19 +54,34 @@ public class DewyActivity extends AppCompatActivity {
 
         });
 
-//        highlight for button
-
+//        highlight for feed button
         btnFeed = (ImageButton) findViewById(R.id.btnFeed);
 
         ((ImageButton)findViewById(R.id.btnFeed)).setOnTouchListener(new View.OnTouchListener() {
 
             @Override
             public boolean onTouch(View v, MotionEvent event) {
+
                 switch (event.getAction()) {
                     case MotionEvent.ACTION_DOWN: {
                         ImageButton view = (ImageButton ) v;
                         view.getBackground().setColorFilter(0x77000000, PorterDuff.Mode.SRC_ATOP);
                         v.invalidate();
+
+                        i += 10;
+                        if ( i < pgExp.getMax() ) {
+                            pgExp.setProgress(i);
+                            ivHeart.setVisibility(View.GONE);
+                        }else {
+                            ivHeart.setVisibility(View.VISIBLE);
+                            pgExp.setProgress(0);
+                            i = 0;
+                            j ++;
+                        }
+
+
+
+                        tvLevel.setText("Lv "+j);
                         break;
                     }
                     case MotionEvent.ACTION_UP:
@@ -64,11 +92,14 @@ public class DewyActivity extends AppCompatActivity {
                         ImageButton view = (ImageButton) v;
                         view.getBackground().clearColorFilter();
                         view.invalidate();
+
                         break;
                     }
                 }
                 return true;
             }
+
         });
+
     }
 }
--- app/src/main/java/com/example/nicha/finalproject/Service/Database.java
+++ app/src/main/java/com/example/nicha/finalproject/Service/Database.java
@@ -27,7 +27,7 @@ import java.util.List;
 public class Database extends SQLiteOpenHelper {
 
     private static final String DB_NAME = "HealthMe";
-    private static final int DB_VERSION = 1;
+    private static final int DB_VERSION = 2;
     SQLiteDatabase mDb;
 
     Context context;
